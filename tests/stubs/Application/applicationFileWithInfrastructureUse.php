2f430e7214a226c39a481182f6d24f7d81e5d61a
Carlos <pardtheman@gmail.com>
2022-02-08 21:03:21 +0100

    application stubs

---
+++ tests/stubs/Application/applicationFileWithInfrastructureUse.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace stubs\Application;
+
+use Throwable;
+use Symfony\Component\Mailer\Mailer;
+use src\Application\Query\Response;
+use src\Domain\ValueObject\UuidIdentity;
+
+class applicationFileWithInfrastructureUse
+{
+}
---
+++ tests/unit/ApplicationFilesSerraTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ApplicationFilesSerraTest extends TestCase
+{
+    private string $serra;
+
+    protected function setUp(): void
+    {
+        $this->serra = dirname(__FILE__) . '/../../src/serra';
+    }
+
+    /**
+     * @test
+     */
+    public function invalid_file_from_application_with_infrastructure_use(): void
+    {
+        $stubFile = dirname(__FILE__) . '/../stubs/Application/applicationFileWithInfrastructureUse.php';
+
+        exec('sudo php '. $this->serra . " -f ". $stubFile, $output, $result);
+
+        $this->assertEquals(1, count($output));
+        $this->assertEquals(1, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function valid_file_from_application(): void
+    {
+        $stubFile = dirname(__FILE__) . '/../stubs/Application/applicationFileWithPhpUsage.php';
+
+        exec('sudo php '. $this->serra . " -f ". $stubFile, $output, $result);
+
+        $this->assertEquals(0, count($output));
+        $this->assertEquals(0, $result);
+    }
+}
